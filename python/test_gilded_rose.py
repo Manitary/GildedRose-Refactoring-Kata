2e16167872f19b0fc6b0851db6dcee79a6aa96f4
Manitary <pak.ska@gmail.com>
2023-01-01 12:36:00 +0000

    Initialise the test suite with a few happy cases

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,50 @@
-# -*- coding: utf-8 -*-
-import unittest
-
+"""Gilded Rose test suite."""
+from typing import Iterator
+import pytest
+from _pytest.mark.structures import ParameterSet
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+def items_are_same(item1: Item, item2: Item) -> bool:
+    """Return whether two Item objects are indistinguishable."""
+    if (
+        item1.name == item2.name
+        and item1.sell_in == item2.sell_in
+        and item1.quality == item2.quality
+    ):
+        return True
+    return False
 
-        
-if __name__ == '__main__':
-    unittest.main()
+
+def item_variants() -> Iterator[ParameterSet]:
+    """Yield the items to test.
+
+    Yield a tuple containing:
+        - An Item object.
+        - The same item, after 1 day has passed."""
+    variants = {
+        "Generic item - new": (
+            Item("+5 Dexterity Vest", 10, 20),
+            Item("+5 Dexterity Vest", 9, 19),
+        ),
+        "Generic item - expiration": (
+            Item("+5 Dexterity Vest", 0, 10),
+            Item("+5 Dexterity Vest", -1, 8),
+        ),
+        "Generic item - min value": (
+            Item("+5 Dexterity Vest", -5, 0),
+            Item("+5 Dexterity Vest", -6, 0),
+        ),
+    }
+    for key, value in variants.items():
+        yield pytest.param(value, id=key)
+
+
+@pytest.mark.parametrize("item", item_variants())
+def test_single_item(item) -> None:
+    """Test a single item after 1 day has passed."""
+    shop = GildedRose(items=[item[0]])
+    shop.update_quality()
+    updated_item = shop.items[0]
+    expected_item = item[1]
+    assert items_are_same(updated_item, expected_item)
