d8c45cebe2bb0f690ed396beced7171d673d4765
Manitary <pak.ska@gmail.com>
2023-01-01 13:00:01 +0000

    Cover the cases of the given example

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -35,6 +35,48 @@ def item_variants() -> Iterator[ParameterSet]:
             Item("+5 Dexterity Vest", -5, 0),
             Item("+5 Dexterity Vest", -6, 0),
         ),
+        "Aged brie - new": (Item("Aged Brie", 2, 0), Item("Aged Brie", 1, 1)),
+        "Aged brie - expiration": (Item("Aged Brie", 0, 2), Item("Aged Brie", -1, 4)),
+        "Aged brie - max value": (
+            Item("Aged Brie", -24, 50),
+            Item("Aged Brie", -25, 50),
+        ),
+        "Sulfuras 1": (
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+        ),
+        "Sulfuras 2": (
+            Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        ),
+        "Backstage pass - new": (
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
+        ),
+        "Backstage pass - 10 days left": (
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 25),
+            Item("Backstage passes to a TAFKAL80ETC concert", 9, 27),
+        ),
+        "Backstage pass - 5 days left": (
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 35),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 38),
+        ),
+        "Backstage pass - expiration": (
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
+        ),
+        "Backstage pass - after expiration": (
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
+            Item("Backstage passes to a TAFKAL80ETC concert", -2, 0),
+        ),
+        "Backstage pass - 10 days left, max value": (
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
+        ),
+        "Backstage pass - 5 days left, max value": (
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 48),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+        ),
     }
     for key, value in variants.items():
         yield pytest.param(value, id=key)
